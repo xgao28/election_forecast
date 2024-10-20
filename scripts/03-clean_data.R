41b329b8252a4c9b182ad81f8654f4c0b255e433
Xinxiang <113380522+xgao28@users.noreply.github.com>
2024-10-20 01:24:38 -0400

    Update clean_data script
    Deleted some unnecessary files

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,65 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleaned the raw data by selecting necessary columns and statistically confident rows, as well as mutate days_towards_election.
+# Author: Xinxiang Gao
+# Date: 20 Oct 2024 
+# Contact: xinxiang.gao@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+data <- read_csv("data/01-raw_data/president_polls.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# Select columns
+selected_columns <- c(
+  "pollster", 
+  "sponsors", 
+  "numeric_grade", 
+  "pollscore", 
+  "methodology", 
+  "transparency_score", 
+  "state", 
+  "start_date", 
+  "end_date", 
+  "sponsor_candidate", 
+  "sponsor_candidate_party", 
+  "question_id", 
+  "sample_size", 
+  "population", 
+  "population_full",
+  "tracking", 
+  "notes", 
+  "source", 
+  "internal", 
+  "partisan", 
+  "race_id", 
+  "ranked_choice_reallocated", 
+  "ranked_choice_round", 
+  "hypothetical", 
+  "party", 
+  "answer", 
+  "candidate_name", 
+  "pct"
+)
+data <- data %>% 
+  select(all_of(selected_columns))
+
+# Select rows
+data <- data %>%
+  filter(numeric_grade >= 2.0)
+
+# Mutate days_towards_election
+data_a <- data %>% 
+  select(end_date) %>% 
+  mutate(end_date = as.Date(end_date, format="%m/%d/%y"))
+
+election_date <- as.Date("11/5/24", format="%m/%d/%y")
+
+data_a <- data_a %>% 
+  mutate(days_towards_election = as.numeric(difftime(election_date, end_date, units = "days")))
+
+data$days_towards_election <- data_a$days_towards_election
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(data, "data/02-analysis_data/cleaned_president_polls.csv")
