8c15568845003ab82c92dadcb1726c6290423dd2
Xinxiang <113380522+xgao28@users.noreply.github.com>
2024-10-21 23:06:00 -0400

    Updated model and clean_data.R

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -11,8 +11,45 @@ library(tidyverse)
 #### Clean data ####
 data <- read_csv("data/01-raw_data/president_polls.csv")
 
+# Select rows
+data <- data %>%
+  filter(numeric_grade >= 2.0)
+
+# Mutate days_towards_election
+data_a <- data %>% 
+  select(end_date) %>% 
+  mutate(end_date = as.Date(end_date, format="%m/%d/%y"))
+
+election_date <- as.Date("11/5/24", format="%m/%d/%y")
+
+data_a <- data_a %>% 
+  mutate(days_towards_election = as.numeric(difftime(election_date, end_date, units = "days")))
+
+
+# TODO: mutate expected_vote
+filtered_data <- data %>%
+  group_by(question_id, poll_id) %>%
+  filter(any(candidate_name == "Kamala Harris") & any(candidate_name == "Donald Trump")) %>%
+  ungroup()
+
+average_pct <- filtered_data %>%
+  filter(candidate_name %in% c("Kamala Harris", "Donald Trump")) %>%
+  group_by(poll_id, candidate_name) %>%
+  summarise(avg_pct = mean(pct), .groups = 'drop')
+
+expected_votes <- average_pct %>%
+  left_join(filtered_data %>% 
+              select(poll_id, sample_size, days_towards_election) %>% 
+              distinct(), 
+            by = "poll_id") %>%
+  mutate(expected_vote = avg_pct * 0.01 * sample_size)
+
+
+
 # Select columns
 selected_columns <- c(
+  "poll_id",
+  "question_id",
   "pollster", 
   "sponsors", 
   "numeric_grade", 
@@ -45,21 +82,8 @@ selected_columns <- c(
 data <- data %>% 
   select(all_of(selected_columns))
 
-# Select rows
-data <- data %>%
-  filter(numeric_grade >= 2.0)
 
-# Mutate days_towards_election
-data_a <- data %>% 
-  select(end_date) %>% 
-  mutate(end_date = as.Date(end_date, format="%m/%d/%y"))
 
-election_date <- as.Date("11/5/24", format="%m/%d/%y")
-
-data_a <- data_a %>% 
-  mutate(days_towards_election = as.numeric(difftime(election_date, end_date, units = "days")))
-
-data$days_towards_election <- data_a$days_towards_election
 
 #### Save data ####
 write_csv(data, "data/02-analysis_data/cleaned_president_polls.csv")
